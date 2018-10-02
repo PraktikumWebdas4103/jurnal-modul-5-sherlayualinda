4279bd204b89ae11b7bf1998e57e51d754d5315b
Sherla yualinda <43510133+sherlayualinda@users.noreply.github.com>
2018-10-02 16:53:21 +0700

    Update komentar.php

--- komentar.php
+++ komentar.php
@@ -1,3 +1,28 @@
-<?php
+<form action=" " method="POST">
+ <center>
+ 	<h1>KOMENTAR</h1>
+ 	<table>
+ 		<tr>
+ 			<td>Inputkan Kata</td>
+ 			<td>:</td>
+ 			<td><input type="textarea" name="kata" ></td>
+ 			<td> <input type="submit" name="submit" value="Cari"></td>
+ 			
+ 		</tr>
+ 	</table>
+</center>
+</form>
 
+<?php
+	if (isset($_POST['submit'])) {
+		$kata =$_POST['kata'];
+		$jmlh = str_word_count($kata);
+
+		if (str_word_count($_POST['kata']) < 5) {
+			echo "komentar harus lebih dari 5 kata";
+		}
+		else{
+			echo "Jumlah kata : $jmlh . </br>";
+		}
+	}
 ?>
